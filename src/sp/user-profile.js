cba068d15ce130f64a6f21ebc1d3c834d4d6e539
John Sucgang <76264041+Sovere7gn@users.noreply.github.com>
2024-02-26 18:43:12 +0800

    Update user-profile.js

--- src/sp/user-profile.js
+++ src/sp/user-profile.js
@@ -1,33 +1,29 @@
-import sp from "../environment/configSPConnection";
+import sp from '../environment/configSPConnection';
 
-export const getCurrentDetails = async () => {
-  let isAdmin = false;
-  let userProps = {};
-  let userSPGroups = [];
+const LIST_NAME = "OfficialDatabase";
 
-  const currentUserGroups = await sp.web.currentUser
-    .select("groups/Title")
-    .expand("groups")();
-  currentUserGroups.Groups.forEach((element) => {
-    if (element.Title === "Administrator") {
-      isAdmin = true;
-    }
-    userSPGroups.push(element.Title);
-  });
 
-  await sp.profiles.myProperties().then((user) => {
-    const newName = user.DisplayName.replace(/ *\([^)]*\) */g, ""); //remove parenthesis'
-    userProps = {
-      Name: user.DisplayName,
-      NewName: newName,
-      Email: user.Email,
-      AccountName: user.AccountName,
-      Office:
-        user.UserProfileProperties.filter((obj) => obj.Key === "Department")[0]
-          .Value || "N/A",
-      Position: user.Title || "N/A",
-      isAdmin: isAdmin,
-    };
-  });
-  return { userProps };
+//get All items
+export const spGetAllItemsToList = async () => {
+   const items = await sp.web.lists.getByTitle(LIST_NAME).items.getAll();
+   return items;
+};
+
+export const spGetItemToList = async (id) => {
+    const items = await sp.web.lists.getByTitle(LIST_NAME).items.getById(id)();
+    return items;
+ };
+
+export const spAddItemToList = async (term, definition, acronym, docuCode, docuTitle, docuLink, effDate) => {
+    const item = await sp.web.lists.getByTitle(LIST_NAME).items.add({field_1: term, field_2: definition, field_4: acronym, field_8: docuCode, field_9: docuTitle, field_10: docuLink, field_11: effDate});
+    return item;
+};
+
+export const spEditItemToList = async (id, term, definition, acronym, docuCode, docuTitle, docuLink, effDate) => {
+    const item = await sp.web.lists.getByTitle(LIST_NAME).items.getById(id).update({field_1: term, field_2: definition, field_4: acronym, field_8: docuCode, field_9: docuTitle, field_10: docuLink, field_11: effDate});
+    return item;
+};
+export const spDeleteItemToList = async (id) => {
+    const item = await sp.web.lists.getByTitle(LIST_NAME).items.getById(id).recycle();
+    return item;
 };
